415347bae456f417e63edf84d852e32adef2737c
NotSleepp <danielfranconot@gmail.com>
2024-03-27 17:11:11 -0300

    terminato a ts

---
+++ src/hooks/useCart.ts
@@ -0,0 +1,84 @@
+import { useEffect, useState, useMemo } from "react"
+import { db } from "../components/data/db"
+import type { Guitar ,CartItem } from "../types"
+
+export const useCart = () => {
+    const initialCart = () : CartItem[] => {
+        const localStorareCart = localStorage.getItem('cart')
+        return localStorareCart ? JSON.parse(localStorareCart) : []
+    }
+
+    const [data] = useState(db)
+    const [cart, setCart] = useState(initialCart)
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
+
+
+    function addToCart(item : Guitar) {
+
+        const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+
+        if (itemExists >= 0) {
+            if (cart[itemExists].quantity >= 5) return
+            const updatedCart = [...cart]
+            updatedCart[itemExists].quantity++
+            setCart(updatedCart)
+        } else {
+            const newItem : CartItem = {...item, quantity : 1}
+            setCart(prevCart => [...prevCart, newItem])
+        }
+
+    }
+
+    function removeFromCart(id : Guitar['id']) {
+        setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
+    }
+
+    function increaseQuantity(id : Guitar['id']) {
+        const updatedCart = cart.map(item => {
+            if (item.id === id && item.quantity < 5) {
+                return {
+                    ...item,
+                    quantity: item.quantity + 1
+                }
+            }
+            return item
+        })
+        setCart(updatedCart)
+    }
+
+    function decreaseQuantity(id : Guitar['id']) {
+        const updatedCart = cart.map(item => {
+            if (item.id === id && item.quantity > 1) {
+                return {
+                    ...item,
+                    quantity: item.quantity - 1
+                }
+            }
+            return item
+        })
+        setCart(updatedCart)
+    }
+
+    function clearCart() {
+        setCart([])
+    }
+
+    const isEmpty = useMemo(() => cart.length === 0, [cart])
+    const cartTotal = useMemo(() => cart.reduce((total, item) => total + (item.quantity * item.price), 0), [cart])
+
+    return {
+        data,
+        cart,
+        addToCart,
+        removeFromCart,
+        decreaseQuantity,
+        increaseQuantity,
+        clearCart,
+        isEmpty,
+        cartTotal
+
+    }
+}
